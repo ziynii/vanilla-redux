11169ddc9646c7f8c33e570ec4c619edfd1ee1bb
Jiyeon <ziyeon.ivi@gmail.com>
2022-08-09 22:09:39 +0900

    ♻️ Refactor countModifier function

--- src/index.js
+++ src/index.js
@@ -4,23 +4,29 @@ const add = document.getElementById('add');
 const minus = document.getElementById('minus');
 const number = document.querySelector('span');
 
+number.innerText = 0;
+
+const ADD = 'ADD';
+const MINUS = 'MINUS'
+
 const countModifier = (count = 0, action) => {
-  if (action.type === 'ADD') {
-    return count + 1;
+  switch (action.type) {
+    case ADD:
+      return count + 1;
+    case MINUS:
+      return count - 1;
+    default:
+      return count;
   }
-  if (action.type === 'MINUS') {
-    return count - 1;
-  }
-  return count;
 };
 
 const countStore = legacy_createStore(countModifier);
 
 const onChange = () => {
-	number.innerText = countStore.getState()
-}
+  number.innerText = countStore.getState();
+};
 
-countStore.subscribe(onChange)
+countStore.subscribe(onChange);
 
 add.addEventListener('click', () => countStore.dispatch({ type: 'ADD' }));
 minus.addEventListener('click', () => countStore.dispatch({ type: 'MINUS' }));
