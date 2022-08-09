43afbc431bf0df4d0750166a5a647763a6e8e0b0
Jiyeon <ziyeon.ivi@gmail.com>
2022-08-09 15:41:26 +0900

    🚧 Add counter function with reducer, subscribe, dispatch

--- src/index.js
+++ src/index.js
@@ -4,9 +4,23 @@ const add = document.getElementById('add');
 const minus = document.getElementById('minus');
 const number = document.querySelector('span');
 
-const countModifier = (count = 0) => {
-	return count
+const countModifier = (count = 0, action) => {
+  if (action.type === 'ADD') {
+    return count + 1;
+  }
+  if (action.type === 'MINUS') {
+    return count - 1;
+  }
+  return count;
 };
 
 const countStore = legacy_createStore(countModifier);
-console.log(countStore.getState());
+
+const onChange = () => {
+	number.innerText = countStore.getState()
+}
+
+countStore.subscribe(onChange)
+
+add.addEventListener('click', () => countStore.dispatch({ type: 'ADD' }));
+minus.addEventListener('click', () => countStore.dispatch({ type: 'MINUS' }));
