19b14cb3d2458b4779f6d298d133eb93fa774926
Jiyeon <ziyeon.ivi@gmail.com>
2022-08-10 00:54:27 +0900

    ♻️ Refactor toDo's add toDo function with redux

--- src/index.js
+++ src/index.js
@@ -1,18 +1,49 @@
+const { legacy_createStore } = require('redux');
+
 const form = document.querySelector('form');
 const input = document.querySelector('input');
 const ul = document.querySelector('ul');
 
-const createToDo = (toDo) => {
-  const li = document.createElement('li');
-  li.innerText = toDo;
-  ul.appendChild(li);
+const ADD_TODO = 'ADD_TODO';
+const DELETE_TODO = 'DELETE_TODO';
+
+const reducer = (state = [], action) => {
+  switch (action.type) {
+    case ADD_TODO:
+      return [{ text: action.text, id: Date.now() }, ...state];
+    case DELETE_TODO:
+      return [];
+    default:
+      return state;
+  }
 };
+
+const store = legacy_createStore(reducer);
+
+store.subscribe(() => console.log(store.getState()))
+
+const paintToDos = () => {
+	const toDos = store.getState();
+	ul.innerHTML = '';
+	toDos.forEach(toDo  => {
+		const li = document.createElement("li")
+		li.id = toDo.id
+		li.innerText = toDo.text
+		ul.appendChild(li)
+	});
+}
+
+store.subscribe(paintToDos)
+
+const addToDo = text => {
+  store.dispatch({ type: ADD_TODO, text });
+}
 
 const onSubmit = (e) => {
   e.preventDefault();
   const toDo = input.value;
   input.value = '';
-  createToDo(toDo);
+	addToDo(toDo)
 };
 
 form.addEventListener('submit', onSubmit);
